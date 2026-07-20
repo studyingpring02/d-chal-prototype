996fcd4d99ee3683b08a1caae1ea12b7369795dc
studyingpring02 <pring02@khu.ac.kr>
2026-07-20 12:44:18 +0900

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 window.DCHAL_SUPABASE_CONFIG = Object.freeze({
   url: 'https://fausmnyyvervjelnvarr.supabase.co',
-  publishableKey: 'sb_publishable_7S1l-v3BNafVJZpZ1BIxIQ_DGQajHL8',
+  publishableKey: 'BLHbCf9GGb1m_Cb2PmeL_D4FfLH16X44f_h4U_Ovlj-xAYE0dCcrZTMVjXlUiYwT5vDgYoVvQb3ih-eN2L78Q3w',
   // Web Push 공개키입니다. 서버 설정 후 생성된 VAPID_PUBLIC_KEY 값을 넣어주세요.
   vapidPublicKey: ''
 });
